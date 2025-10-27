d2812977e1384985587c8574f355bc63d061f856
siyuuhhhh <7siyuzi@proton.me>
2025-10-27 10:59:15 +0545

    Update and rename premium.lua to loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+--[[
+█▀▀ █░░ █▀█ █░█ █▀▄ █░█░█ ▄▀█ █▀█ █▀▀
+█▄▄ █▄▄ █▄█ █▄█ █▄▀ ▀▄▀▄▀ █▀█ █▀▄ ██▄
+dsc.gg/xcloudware
+]]--
+
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/93e45de806a845acbf2a49915f64ffc9"))();
